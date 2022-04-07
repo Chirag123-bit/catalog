344149819f8e642d3f0b711a0b35050f4293c899
Chirag Simkhada <chiragsimkhada@gmail.com>
2022-04-07 21:08:39 +0545

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:catalog/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+      themeMode: ThemeMode.dark,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+    );
+  }
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+// ignore: use_key_in_widget_constructors
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Catalog App'),
+      ),
+      // ignore: prefer_const_constructors
+      body: Center(
+        child: const Text("Hello World"),
+      ),
+    );
+  }
+}
